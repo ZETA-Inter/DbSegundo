198e5304edeab13c61eeab4bac5b0460289868ba
SofiaRSantana <sofia.santana@germinare.org.br>
2025-09-04 21:46:34 -0300

    Modificando campos e ordem das tabelas

--- script_000.sql
+++ script_000.sql
@@ -1,3 +1,22 @@
+DROP TABLE IF EXISTS plan_functionalities;
+DROP TABLE IF EXISTS functionalities;
+DROP TABLE IF EXISTS images;
+DROP TABLE IF EXISTS responsibles;
+DROP TABLE IF EXISTS goals;
+DROP TABLE IF EXISTS worker_programs;
+DROP TABLE IF EXISTS progresses;
+DROP TABLE IF EXISTS user_segments;
+DROP TABLE IF EXISTS workers;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS plans;
+DROP TABLE IF EXISTS programs;
+DROP TABLE IF EXISTS segments;
+
+CREATE TABLE segments ( id   SERIAL       NOT NULL UNIQUE      
+                      , name VARCHAR(255) NOT NULL      
+                      , PRIMARY KEY (id)
+                      );
+					  
 CREATE TABLE programs ( id          SERIAL       NOT NULL UNIQUE      
                       , name        VARCHAR(255) NOT NULL      
                       , description VARCHAR(255) NOT NULL      
@@ -5,6 +24,12 @@ CREATE TABLE programs ( id          SERIAL       NOT NULL UNIQUE
                       , PRIMARY KEY (id)      
                       , FOREIGN KEY (segment_id) REFERENCES segments(id)
                       );
+
+CREATE TABLE plans ( id                    SERIAL       NOT NULL UNIQUE      
+                   , name                  VARCHAR(255) NOT NULL
+                   , value                 DECIMAL           NOT NULL      
+                   , PRIMARY KEY(id)
+                   );
 
 CREATE TABLE companies ( id       SERIAL       NOT NULL UNIQUE       
                        , name     VARCHAR(255) NOT NULL       
@@ -29,11 +54,6 @@ CREATE TABLE workers ( id         SERIAL       NOT NULL UNIQUE
                      );
 
 
-CREATE TABLE segments ( id   SERIAL       NOT NULL UNIQUE      
-                      , name VARCHAR(255) NOT NULL      
-                      , PRIMARY KEY (id)
-                      );
-
 CREATE TABLE user_segments ( id         SERIAL  NOT NULL UNIQUE        
                            , segment_id INTEGER NOT NULL        
                            , company_id INTEGER     NULL        
@@ -44,11 +64,15 @@ CREATE TABLE user_segments ( id         SERIAL  NOT NULL UNIQUE
                            , FOREIGN KEY (worker_id)  REFERENCES workers(id)
                            );
 
-CREATE TABLE plans ( id                    SERIAL       NOT NULL UNIQUE      
-                   , name                  VARCHAR(255) NOT NULL
-                   , value                 DECIMAL           NOT NULL      
-                   , PRIMARY KEY(id)
-                   );
+CREATE TABLE progresses ( id                  SERIAL  NOT NULL UNIQUE 
+                        , date                DATE     
+                        , points              INTEGER     
+                        , progress_percentage INTEGER 
+                        , worker_id           INTEGER NOT NULL 
+                        , PRIMARY KEY (id)
+                        , FOREIGN KEY (worker_id) REFERENCES workers (id)
+                        );
+						
 
 CREATE TABLE worker_programs ( id          SERIAL  NOT NULL UNIQUE       
                              , worker_id   INTEGER NOT NULL       
@@ -60,14 +84,6 @@ CREATE TABLE worker_programs ( id          SERIAL  NOT NULL UNIQUE
                              , FOREIGN KEY (program_id)  REFERENCES programs(id)       
                              , FOREIGN KEY (progress_id) REFERENCES progresses(id)
                              );
-
-CREATE TABLE progresses ( id                  SERIAL       NOT NULL UNIQUE     
-                        , name                VARCHAR(255) NOT NULL     
-                        , date                DATE     
-                        , points              INTEGER     
-                        , progress_percentage INTEGER     
-                        , PRIMARY KEY (id)
-                        );
 
 CREATE TABLE goals ( id                SERIAL       NOT NULL UNIQUE      
                    , worker_program_id INTEGER      NOT NULL      
@@ -92,6 +108,11 @@ CREATE TABLE images ( id           SERIAL       NOT NULL UNIQUE
                     , PRIMARY KEY(id)
                     );
 
+CREATE TABLE functionalities ( id   SERIAL       NOT NULL UNIQUE       
+                             , name VARCHAR(255)       
+                             , PRIMARY KEY (id)
+                             );					
+
 CREATE TABLE plan_functionalities ( id               SERIAL  NOT NULL UNIQUE         
                                   , plan_id          INTEGER NOT NULL         
                                   , functionality_id INTEGER NOT NULL         
@@ -99,9 +120,4 @@ CREATE TABLE plan_functionalities ( id               SERIAL  NOT NULL UNIQUE
                                   , FOREIGN KEY (plan_id)          REFERENCES plans(id)         
                                   , FOREIGN KEY (functionality_id) REFERENCES functionalities(id)
                                   );
-
-CREATE TABLE functionalities ( id   SERIAL       NOT NULL UNIQUE       
-                             , name VARCHAR(255)       
-                             , PRIMARY KEY (id)
-                             );
 
