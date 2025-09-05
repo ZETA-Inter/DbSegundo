973337332f58f4520165e82d0aafe872597e94b2
SofiaRSantana <sofia.santana@germinare.org.br>
2025-09-04 21:50:32 -0300

    Adicionando procedure 1 e function 1

---
+++ script_002.sql
@@ -0,0 +1,55 @@
+-- PROCEDURE
+-- Criar um novo programa
+CREATE OR REPLACE PROCEDURE sp_create_program (
+											     pProgramName VARCHAR,
+												 pDescription VARCHAR,
+												 pSegmentId INT
+											   )
+LANGUAGE plpgsql AS $$
+BEGIN
+
+	IF NOT EXISTS (
+		SELECT id FROM segments
+		WHERE id = pSegmentId
+	) THEN
+		RAISE EXCEPTION 'Este segmento não existe. Insira um ID válido';
+	END IF;
+
+	INSERT INTO programs VALUES (
+		DEFAULT, 
+		pProgramName, 
+		pDescription, 
+		pSegmentId);
+
+EXCEPTION
+	WHEN not_null_violation THEN	
+		RAISE EXCEPTION 'Não é permitido inserção de valores nulos.';
+END
+$$;
+
+-- FUNCTION
+-- Tem como parâmetro o id da empresa company) e retorna os responsáveis dela
+CREATE OR REPLACE FUNCTION fn_responsibles_per_company (pCompanyId INTEGER)
+RETURNS TABLE ( email VARCHAR,
+			    first_name VARCHAR,
+				last_name VARCHAR,
+				company_id INTEGER
+			  )
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	RETURN QUERY
+		SELECT 
+			r.email, 
+			r.first_name,
+			r.last_name,
+			r.company_id
+		FROM responsibles r
+		WHERE r.company_id = pCompanyId;
+
+	IF NOT FOUND THEN
+		RAISE EXCEPTION 'Não existe este ID da companhia. Tente um ID válido';
+	END IF;
+
+END;
+$$
