8fd6fb0976f3da7deb2f41e0cd3e4ffad0c5eeb5
sosogasp <sophia.gasparetto.2009@gmail.com>
2025-10-02 08:42:18 -0300

    criando fn_goals_by_worker

---
+++ script_010.sql
@@ -0,0 +1,24 @@
+ALTER TABLE goals
+DROP COLUMN completed;
+
+ALTER TABLE worker_goals
+ADD COLUMN completed BOOLEAN NOT NULL DEFAULT false;
+
+CREATE OR REPLACE FUNCTION fn_goals_by_worker(pWorkerId INTEGER)
+RETURNS TABLE (
+    goal      VARCHAR(50),
+    completed BOOLEAN
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT 
+        g.description AS goal,
+        wg.completed  AS completed,
+    FROM 
+        goals g
+    JOIN worker_goals wg ON g.id = wg.goal_id
+    WHERE wg.worker_id = pWorkerId;
+END;
+$$;
