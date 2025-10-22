f7c1a2bed71cd93bf66cfa2d5ad31c2cb54e5e26
sosogasp <sophia.gasparetto.2009@gmail.com>
2025-10-22 06:51:55 -0600

    corrigindo trigger

--- script_029.sql
+++ script_029.sql
@@ -3,12 +3,12 @@ RETURNS TRIGGER AS $$
 BEGIN
     IF TG_OP = 'DELETE' THEN
         DELETE FROM worker_goals WHERE goal_id = OLD.id;
-        RETURN NULL;
+        RETURN OLD;  -- It's usually a good practice to return the OLD record in a delete trigger.
     END IF;
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER tg_delete_goal
-AFTER DELETE ON goals
+BEFORE DELETE ON goals
 FOR EACH ROW EXECUTE FUNCTION trg_delete_goal();
