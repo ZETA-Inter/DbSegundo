3d1f29db6074c44d92924f908562f461ced8c162
SofiaRSantana <sofia.santana@germinare.org.br>
2025-10-21 22:30:13 -0300

    Modificando o segundo IF da procedure

--- script_028.sql
+++ script_028.sql
@@ -9,9 +9,11 @@ BEGIN
 		RAISE EXCEPTION 'Erro: company_id inválido';
 	END IF;
 
-	IF NOT EXISTS (SELECT 1 FROM worker_programs wp JOIN workers w ON w.id = wp.worker_id WHERE w.company_id = pCompanyId and wp.program_id = pProgramId) THEN
-		RAISE EXCEPTION 'Erro: não existem produtores com esse curso';
-	END IF; 
+	IF pProgramId EXISTS THEN
+		IF NOT EXISTS (SELECT 1 FROM worker_programs wp JOIN workers w ON w.id = wp.worker_id WHERE w.company_id = pCompanyId and wp.program_id = pProgramId) THEN
+			RAISE EXCEPTION 'Erro: não existem produtores com esse curso';
+		END IF; 
+	END IF;
 
 	IF EXISTS (SELECT 1 FROM goals WHERE description = pDescription AND company_id = pCompanyId) THEN
 		RAISE EXCEPTION 'Erro: Não é possível criar uma meta com uma descrição igual a de uma meta pré-existente';
